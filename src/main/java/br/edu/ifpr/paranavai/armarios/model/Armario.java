33b060f9f5d99de5c6a0ea8672f72453ba004643
Gabriel Silva <gabrielsilvaol10@gmail.com>
2022-09-01 16:51:23 -0300

    Iniciando configuração da conexão com o BD

--- src/main/java/br/edu/ifpr/paranavai/armarios/model/Armario.java
+++ src/main/java/br/edu/ifpr/paranavai/armarios/model/Armario.java
@@ -1,39 +1,16 @@
 package br.edu.ifpr.paranavai.armarios.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Armario {
     
     private String numero;
     private boolean ativo;
     private String observacoes;
-    
-    public Armario() {}
-
-    public Armario(String numero, boolean ativo, String observacoes) {
-        this.numero = numero;
-        this.ativo = ativo;
-        this.observacoes = observacoes;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
-
-    public String getObservacoes() {
-        return observacoes;
-    }
-
-    public void setObservacoes(String observacoes) {
-        this.observacoes = observacoes;
-    }
 }
